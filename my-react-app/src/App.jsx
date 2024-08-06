b77af22a20c2e0712c08d311f5ffdef67e20f931
Florian <florian-haeglsperger@gmx.de>
2024-08-06 17:07:43 +0200

    first-commit

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,14 +1,12 @@
-import Card from "./Card.jsx";
+import Button from './Button.jsx';
 
 function App() {
 
     return(
       <>
-        <Card></Card>
-        <Card></Card>
         
+        <Button></Button>
       </>
-      
     );
 }
 
